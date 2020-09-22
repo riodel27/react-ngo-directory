82f8171fc8556215ed45f4ff5225fa1523ad689f
rpamaong <rpamaong@startechup.com>
2020-09-22 20:57:47 +0800

    added Dashboard components

---
+++ src/components/Dashboard/styled.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+});
+
+export const useStylesOrder = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+}));
