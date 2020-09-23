8aadb66b54d2ce690c91716b693dd5ebe240c00b
rpamaong <rpamaong@startechup.com>
2020-09-23 16:52:42 +0800

    added get users query hook

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {
-  Container,
-  CssBaseline,
-  IconButton,
-  Typography,
-} from "@material-ui/core";
-
-import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
+import { Container, CssBaseline, Typography } from "@material-ui/core";
 
 import { useStyles } from "./styled";
+import useUsersQuery from "hooks/user/query/useUsersQuery";
 
 interface UserProps {}
 
 export const User: React.FC<UserProps> = ({}) => {
   const classes = useStyles();
+
+  const { isLoading, data, isError } = useUsersQuery();
+
+  if (isLoading) return <>loading...</>;
+
+  if (isError) return <>"An error has occurred..."</>;
 
   return (
     <>
@@ -22,10 +22,11 @@ export const User: React.FC<UserProps> = ({}) => {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography variant="h3" align="center" color="textPrimary">
-              <span>Users</span>
-              <IconButton aria-label="add">
-                <AddCircleOutlinedIcon />
-              </IconButton>
+              <span> Users</span>
+              <ul>
+                {data &&
+                  data.map((user: any) => <li key={user._id}>{user.email}</li>)}
+              </ul>
             </Typography>
           </Container>
         </div>
