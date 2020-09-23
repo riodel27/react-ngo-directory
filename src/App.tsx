3cf1c7695046a1c32655ada1374d3f1d88e42785
rpamaong <rpamaong@startechup.com>
2020-09-23 15:26:36 +0800

    move react-query init to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryCache, ReactQueryCacheProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
 
 import Header from "components/Header";
 import Routes from "routes";
 
+const queryCache = new QueryCache();
+
 function App() {
   return (
-    <>
+    <ReactQueryCacheProvider queryCache={queryCache}>
       <Router>
         <Header />
         <Routes />
       </Router>
-      <ReactQueryDevtools />
-    </>
+      <ReactQueryDevtools initialIsOpen />
+    </ReactQueryCacheProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { QueryCache, ReactQueryCacheProvider } from "react-query";
 
 import { AuthProvider } from "context/auth";
 
 import App from "./App";
 
-const queryCache = new QueryCache();
-
 ReactDOM.render(
   <React.StrictMode>
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ReactQueryCacheProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
