4561e783c375010dfc4999c71b8dd9cb3afb88c0
rpamaong <rpamaong@startechup.com>
2020-09-30 23:47:35 +0800

    added error boundary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import { QueryCache, ReactQueryCacheProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
 
-import { FullPageSpinner } from "components/lib";
 import Header from "components/Header";
+import {
+  FullPageErrorFallback,
+  FullPageSpinner,
+  ErrorFallback,
+} from "components/lib";
 
 const Routes = React.lazy(() => import("routes"));
 
 const queryCache = new QueryCache();
 
-//TODO: implement loading and error handling
-//TODO: implement react-query backround prefetch status...
-
 function App() {
   return (
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <Router>
-        <Header />
-        <React.Suspense fallback={<FullPageSpinner />}>
-          <Routes />
-        </React.Suspense>
-      </Router>
-      <ReactQueryDevtools initialIsOpen />
-    </ReactQueryCacheProvider>
+    <ErrorBoundary FallbackComponent={FullPageErrorFallback}>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <Router>
+          <Header />
+          <React.Suspense fallback={<FullPageSpinner />}>
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <Routes />
+            </ErrorBoundary>
+          </React.Suspense>
+        </Router>
+        <ReactQueryDevtools initialIsOpen />
+      </ReactQueryCacheProvider>
+    </ErrorBoundary>
   );
 }
 
