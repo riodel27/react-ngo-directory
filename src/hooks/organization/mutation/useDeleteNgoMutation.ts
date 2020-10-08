a305632b521d707475f9441ef3d39aa5f4dc40c1
rpamaong <rpamaong@startechup.com>
2020-10-08 23:23:26 +0800

    refactor: optimistic update on use delete and update organization hook

--- src/hooks/organization/mutation/useDeleteNgoMutation.ts
+++ src/hooks/organization/mutation/useDeleteNgoMutation.ts
@@ -1,15 +1,36 @@
 import { axiosInstance } from '_services/base';
 import { useMutation, useQueryCache } from 'react-query';
 
-//TODO: optimistic update
 export default function useDeleteNgoMutation() {
+   const cache = useQueryCache();
    return useMutation(
       async (id) => {
          await axiosInstance.delete(`/organization/${id}`);
          return;
       },
       {
-         onSuccess: () => console.log('success delete organization')
+         onSuccess: () => console.log('success delete organization'),
+         onMutate: (deleted_ngo) => {
+            // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+            cache.cancelQueries('organizations');
+
+            // Snapshot the previous value
+            const previous_organizations = cache.getQueryData('organizations');
+
+            // Optimistically update to the new list of organizations
+            cache.setQueryData('organizations', (old_ngos: any) =>
+               old_ngos.filter((ngo: any) => ngo._id !== deleted_ngo)
+            );
+
+            // Return the snapshotted value
+            return () => cache.setQueryData('organizations', previous_organizations);
+         },
+         // If the mutation fails, use the value returned from onMutate to roll back
+         onError: (_, __, rollback: any) => rollback(),
+         // Always refetch after error or success:
+         onSettled: () => {
+            cache.invalidateQueries('organizations');
+         }
       }
    );
 }
--- src/hooks/organization/mutation/useUpdateNgoMutation.ts
+++ src/hooks/organization/mutation/useUpdateNgoMutation.ts
@@ -3,15 +3,42 @@ import { useMutation, useQueryCache } from 'react-query';
 
 import { OrganizationInput } from '@/global/types';
 
-//TODO: optimistic update
 export default function useUpdateNgoMutation() {
+   const cache = useQueryCache();
    return useMutation(
       async (data: OrganizationInput | null) => {
          await axiosInstance.put(`/organization/${data?.id}`, data);
          return;
       },
       {
-         onSuccess: () => console.log('success update organization')
+         onSuccess: () => console.log('success update organization'),
+         onMutate: (updated_ngo) => {
+            // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+            cache.cancelQueries('organizations');
+
+            // Snapshot the previous value
+            const previous_organizations = cache.getQueryData('organizations');
+
+            // Optimistically update to the new list of organizations
+            cache.setQueryData('organizations', (old_ngos: any) => {
+               const elementsIndex = old_ngos.findIndex((ngo: any) => ngo._id === updated_ngo?.id);
+
+               const new_ngos = [...old_ngos];
+
+               new_ngos[elementsIndex] = { ...new_ngos[elementsIndex], ...updated_ngo };
+
+               return new_ngos;
+            });
+
+            // Return the snapshotted value
+            return () => cache.setQueryData('organizations', previous_organizations);
+         },
+         // If the mutation fails, use the value returned from onMutate to roll back
+         onError: (err, updated_ngo, rollback: any) => rollback(),
+         // Always refetch after error or success:
+         onSettled: () => {
+            cache.invalidateQueries('organizations');
+         }
       }
    );
 }
